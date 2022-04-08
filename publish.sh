50d5632dc04eaa222e7449e3f0b92ecd4ac0ac43
root <max.marie.4@gmail.com>
2022-04-08 09:35:06 +0000

    update publish file

--- publish.sh
+++ publish.sh
@@ -1,8 +1,9 @@
-echo "1/4 ********************* Pull latest changes *********************"
+echo -e "\n\n[1/4] ********************* Pull latest changes *********************\n\n"
 git pull
-echo "2/4 ********************* Kill running app ************************"
+echo -e "\n\n[2/4] ********************* Kill running app ************************\n\n"
 pkill -f "./HeroicBrawlServer/bin/Release/net6.0/HeroicBrawlServer"
-echo "3/4 ********************* Build release ***************************"
+echo OK.
+echo -e "\n\n[3/4] ********************* Build release ***************************\n\n"
 dotnet publish -c Release
-echo "4/4 ********************* Launch app ******************************"
+echo -e "\n\n[4/4] ********************* Launch app ******************************\n\n"
 nohup ./HeroicBrawlServer/bin/Release/net6.0/HeroicBrawlServer &
